3c61beb785a7252d935ac6389206b0e006e7bd86
lxhem <143767958+lxhem@users.noreply.github.com>
2024-07-09 13:29:52 -0400

    update

--- script.ps1
+++ script.ps1
@@ -3,12 +3,25 @@
 # Select Subscription
 # Remove the tags
 
-# Automation Account and Resource Group
-$automationAccountName = "aaName"   #replace later
-$resourceGroupName = "rgName"   #replace later
+Connect-AzAccount
+
+# Choose tenant (update later)
+Get-AzTenant #display a list to select
+Select-AzTenant -TenantId "TenantId"
+
+# Choose subscritiption
+Get-AzSubscription #display list to select
+Select-AzSubscription -SubscriptionName "YourSubscriptionName"
+
+# Choose Resource Group
+# Ensure the right rg is selected
+$rgName = "resourceGroupName"   #replace later with UI
+
+# Get automation account
+$automationAccount = Get-AzAutomationAccount -ResourceGroupName $rgName 
 
 # Get variables in Automation Account
-$variables = Get-AzAutomationVariable -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName
+$variables = Get-AzAutomationVariable -ResourceGroupName $rgName -AutomationAccountName $automationAccount
 
 # Iterate throught list of variables
 Foreach ($variable in $variables) {
